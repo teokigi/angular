943456a4d123229321b92436168c618102e07178
Teo <43215755+teokigi@users.noreply.github.com>
2020-08-11 21:41:16 +0100

    created customer service

---
+++ src/app/home/customer.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(
+      private http: HttpClient
+  ) { }
+
+  getCustomers(){
+      return this.http.get('/assets/shipping.json');
+  }
+
+}
