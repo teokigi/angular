84901ea17e4ea14da2774c1caaafbb87967f5d2d
Teo <43215755+teokigi@users.noreply.github.com>
2020-08-11 18:06:44 +0100

    generated customer and contractor components instead of modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CustomersComponent } from './home/customers/customers.component';
+import { ContractorsComponent } from './home/contractors/contractors.component';
+import { AuthComponent } from './auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'home/customers', component: CustomersComponent},
+    { path: 'home/contractors', component: ContractorsComponent},
+    { path: 'auth', component: AuthComponent},
+    { path: '', redirectTo: '/home-component', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,13 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
-  template: `
-    <p>
-      auth works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
 
