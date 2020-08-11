6436d06b6d7185c4b8f35b671f3c04968e697864
Teo <43215755+teokigi@users.noreply.github.com>
2020-08-11 21:19:42 +0100

    added component import customer and contractor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { CustomersComponent } from './home/customers/customers.component';
+import { ContractorsComponent } from './home/contractors/contractors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AuthComponent
+    AuthComponent,
+    CustomersComponent,
+    ContractorsComponent,
   ],
   imports: [
     BrowserModule,
