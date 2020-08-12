03030d117d09882bfb8aec7ea9a8e3c69598c6c9
Teo <43215755+teokigi@users.noreply.github.com>
2020-08-12 15:13:40 +0100

    installed ng zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { YourGuardGuard } from './auth/your-auth.guard';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { uk_UA } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import uk from '@angular/common/locales/uk';
+
+registerLocaleData(uk);
 
 @NgModule({
   declarations: [
@@ -15,8 +24,11 @@ import { YourGuardGuard } from './auth/your-auth.guard';
     BrowserModule,
     AppRoutingModule,
     HomeModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [YourGuardGuard],
+  providers: [YourGuardGuard, { provide: NZ_I18N, useValue: uk_UA }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
