99ebeab222175a2b929f1484a342acef64c76d2a
Teo <43215755+teokigi@users.noreply.github.com>
2020-08-12 14:57:55 +0100

    removing fluff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 import { YourGuardGuard } from './auth/your-auth.guard';
 
 const routes: Routes = [
